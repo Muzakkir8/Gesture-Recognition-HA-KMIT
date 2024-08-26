31400e83219e3f359a09dbb464ad9169ff00512f
Akifali-1 <akifalilenovo@gmail.com>
2024-08-26 19:07:14 +0530

    hiii

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert("This is G160")
+var loginBtn = document.getElementById("loginbtn");
+var registerBtn = document.getElementById("registerbtn");
+var loginForm = document.getElementById("login");
+var registrationForm = document.getElementById("registration1");
+
+function login() {
+    loginForm.style.left = "5px";
+    registrationForm.style.right = "-520px";
+    loginBtn.classList.add("whitebtn");
+    registerBtn.classList.remove("whitebtn");
+}
+
+function register() {
+    loginForm.style.left = "-510px";
+    registrationForm.style.right = "5px";
+    registerBtn.classList.add("whitebtn");
+    loginBtn.classList.remove("whitebtn");
+}

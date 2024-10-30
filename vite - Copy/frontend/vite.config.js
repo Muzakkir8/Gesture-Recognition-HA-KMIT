570eaa8c79924e53eda2604000165c36e8a09b97
Zeeshan <zeeshanar56@gmail.com>
2024-10-30 10:15:16 +0530

    Working

--- vite - Copy/frontend/vite.config.js
+++ vite - Copy/frontend/vite.config.js
@@ -6,14 +6,7 @@ export default defineConfig({
   css: {
     postcss: './postcss.config.js',
     server: {
-      host: '0.0.0.0', // Allows access from any IP in your network
-      port: 8000,
-      proxy: {
-        '/api': {
-          target: 'http://192.168.97.145:8000', // Your backend server
-          changeOrigin: true,
-        },
-      },   
+      port:8000,
       historyApiFallback: true,},  // This points to your PostCSS config
   }
 })
